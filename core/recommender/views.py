61aa6d7397ba7be6b6ab5e05103f529af54e3c07
sarfras <sarfrasspc@gmail.com>
2025-06-18 21:47:35 +0530

    create project folder and app

---
+++ core/recommender/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+import os
+
+CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
+CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
+REDIRECT_URI = os.getenv('SPOTIPY_REDIRECT_URI')
